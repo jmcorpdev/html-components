a499883ac7a279dd86853cda499db2dd23fd4435
Arnaud Gueras <arnaudgs@gmail.com>
2022-08-09 08:17:33 +0200

    Fix all npm vulnerabilities

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,79 @@
-'use strict';
+"use strict";
 
 module.exports = function (grunt) {
-    // Show elapsed time at the end
-    //require('time-grunt')(grunt);
-    // Load all grunt tasks
-    require('load-grunt-tasks')(grunt);
+  // Show elapsed time at the end
+  //require('time-grunt')(grunt);
+  // Load all grunt tasks
+  require("load-grunt-tasks")(grunt);
 
-    // Project configuration.
-    grunt.initConfig({
-        clean: ['.tmp'],
-        nodeunit: {
-            files: ['test/**/*_test.js']
+  // Project configuration.
+  grunt.initConfig({
+    clean: [".tmp"],
+    nodeunit: {
+      files: ["test/**/*_test.js"],
+    },
+    jshint: {
+      options: {
+        jshintrc: ".jshintrc",
+        reporter: require("jshint-stylish"),
+      },
+      gruntfile: {
+        src: "Gruntfile.js",
+      },
+      lib: {
+        src: ["lib/!**!/!*.js"],
+      },
+      test: {
+        src: ["test/!**!/!*.js"],
+      },
+    },
+    // mochacli: {
+    //     options: {
+    //         reporter: 'spec',
+    //         bail: true
+    //     },
+    //     all: ['test/*.js']
+    // },
+    jsdox: {
+      generate: {
+        options: {
+          contentsTitle: "My Project Documentation",
         },
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            lib: {
-                src: ['lib/!**!/!*.js']
-            },
-            test: {
-                src: ['test/!**!/!*.js']
-            }
-        },
-        mochacli: {
-            options: {
-                reporter: 'spec',
-                bail: true
-            },
-            all: ['test/*.js']
-        },
-        jsdox: {
-            generate: {
-                options: {
-                    contentsTitle: 'My Project Documentation'
-                },
 
-                src: ['lib/**/*.js'],
-                dest: 'doc'
-            }
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            lib: {
-                files: '<%= jshint.lib.src %>',
-                tasks: ['jshint:lib', 'mochacli']
-            },
-            test: {
-                files: '<%= jshint.test.src %>',
-                tasks: ['mochacli']
-            }
-//            example:{
-//                files: 'example/**/*.*',
-//                tasks: ['example']
-//            }
-        }
-    });
+        src: ["lib/**/*.js"],
+        dest: "doc",
+      },
+    },
+    watch: {
+      gruntfile: {
+        files: "<%= jshint.gruntfile.src %>",
+        tasks: ["jshint:gruntfile"],
+      },
+      lib: {
+        files: "<%= jshint.lib.src %>",
+        tasks: ["jshint:lib", "mochacli"],
+      },
+      test: {
+        files: "<%= jshint.test.src %>",
+        tasks: ["mochacli"],
+      },
+      //            example:{
+      //                files: 'example/**/*.*',
+      //                tasks: ['example']
+      //            }
+    },
+  });
 
-    // Default task.
-    grunt.registerTask('default', ['clean', 'mochacli', 'clean']);
-    grunt.registerTask('dev', ['example', 'watch']);
-    grunt.registerTask('devtest', ['mochacli', 'watch']);
-    /*grunt.registerTask('example', function () {
+  // Default task.
+  grunt.registerTask("default", ["clean", "mochacli", "clean"]);
+  grunt.registerTask("dev", ["example", "watch"]);
+  grunt.registerTask("devtest", ["mochacli", "watch"]);
+  /*grunt.registerTask('example', function () {
      var example = require('./example/html-components_example');
      example();
      });
      */
-    /*grunt.registerTask('docEnd', function() {
+  /*grunt.registerTask('docEnd', function() {
      //move generated document files into doc folder
      var fs = require('fs');
      if(!fs.existsSync('doc')) {
--- README.md
+++ README.md
@@ -1,41 +1,44 @@
 > Create html components from templates in a simple way
 
+## NPM 8 mandatory
+
+This package use the overrides propertie available in NPM 8.
+Please update your NPM version to 8.x or higher if you want to use this package.
 
 ## Getting Started
 
 Install the module with: `npm install html-components --save-dev`
 
 ```js
-var HtmlComponents = require('html-components');
+var HtmlComponents = require("html-components");
 var htmlComponent = new HtmlComponents({
-    componentsFolder:'components',
+  componentsFolder: "components",
 });
 ```
 
 ##usage
-
 
 ## Documentation
 
 All methods are described here :
 [lib/html-components.js](doc/lib/html-components.md)
 
-
 ## Examples
+
 - Create a component into the components folder
-components/mycomp.hbs
+  components/mycomp.hbs
 
 ```html
 <div class="mycomp">
     {{{myattr}}}
-    
+
     {{#if attr2}}
         <span>I am another attribute : {{attr2}}
     {{/if}}
 </div>
 ```
 
-Use it in a html page 
+Use it in a html page
 
 ```html
 <!DOCTYPE html>
@@ -52,7 +55,8 @@ Use it in a html page
 </html>
 ```
 
-Result : 
+Result :
+
 ```html
 <!DOCTYPE html>
 <html>
@@ -60,35 +64,31 @@ Result :
 
 <div class="mycomp">xwxx
     custom attribute
-    
+
     <span>I am another attribute : I can also use the attribute<br> with html or another custom html <strong>components</strong>
-        
+
 </div>
 
 </body>
 </html>
 ```
 
-
-
-
-
 ## Contributing
+
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com).
 
-
 # Todo list
-* BUG : if component file is call "line.hbs", it doesn't work
-* TODO : add possibility to create data-dummy and data-foo object in addition to the data object. example : <node data-dummy-bar="foo" data-dummy-fooooo="bar"> become an object data-dummy and a string if necessary
-* TODO : add cache for templates
-* TODO : extract handlebars as a plugin
-* TODO : manage boolean attributes (if attribute="false" or attribute="true" become boolean)
-* TODO : manage single attributes like <node attribute required nothing readonly></node>
-* TODO : add option to remove empty lines when there are more than 2 lines
-* TODO : add option for template language (possibility to replace handlebars by another template engine)
-* TODO : check if code formatting can be added easily
-* TODO : documentation
- 
+
+- BUG : if component file is call "line.hbs", it doesn't work
+- TODO : add possibility to create data-dummy and data-foo object in addition to the data object. example : <node data-dummy-bar="foo" data-dummy-fooooo="bar"> become an object data-dummy and a string if necessary
+- TODO : add cache for templates
+- TODO : extract handlebars as a plugin
+- TODO : manage boolean attributes (if attribute="false" or attribute="true" become boolean)
+- TODO : manage single attributes like <node attribute required nothing readonly></node>
+- TODO : add option to remove empty lines when there are more than 2 lines
+- TODO : add option for template language (possibility to replace handlebars by another template engine)
+- TODO : check if code formatting can be added easily
+- TODO : documentation
 
 ## License
 
